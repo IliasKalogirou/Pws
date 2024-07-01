64eaa90cfc6cf3875fbab86ae9603331fb0ba6b3
Rayaneschool <151843713+Rayaneschool@users.noreply.github.com>
2024-07-01 13:01:45 +0000

    achtergrond

--- main.py
+++ main.py
@@ -8,30 +8,58 @@ pygame.init()
 
 pygame.display.set_caption("platformer")
 
-BG_COLOR = (255,255,255)
 WIDTH, HEIGHT = 250, 250
 FPS = 60
-PLAYER_VEL = 5
+PLAYER_SPEED = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+#Achtergrond gaat over de hele scherm
+def get_background(name):
+    image = pygame.image.load(join("achtergrond", "galaxy.png"))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+     for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+    return tiles, image
+
+
+#Steeds nieuwe achtergrond tekenen bij elke beweging
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_inage, tile)
+
+    pygame.display.update()
 
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("galaxy.png")
 
-#Deze code hieronder zorgt ervoor dat de game niet meer dan het aantal ingestelde fps (in dit geval 60) runt.
+#While loop
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                break
+        
+        draw(window, background, bg_image)  
     
+    #Juiste fps
     clock.tick(FPS)
+    
+    pygame.quit()
+    quit()
+
 
     # Keyboard inputs
     keys = pygame.key.get_pressed()
     if keys[pygame.K_ESCAPE]:
         pygame.quit()
+
 
 
 
